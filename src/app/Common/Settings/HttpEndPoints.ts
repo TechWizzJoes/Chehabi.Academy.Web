ee0ebcb8c2936e7b269b91797642dbaed0914486
youssefmansour <ymansour@ultatel.com>
2023-10-30 23:29:20 +0200

    WEB-16
    add google login in client and adjust navbar accordingly

--- src/app/Common/Settings/HttpEndPoints.ts
+++ src/app/Common/Settings/HttpEndPoints.ts
@@ -3,6 +3,9 @@ export class HttpEndPoints {
 
 	public static Account = {
 		Login: 'account/login',
+		SocialLogin: {
+			Google: 'account/google-login'
+		},
 		Register: 'account/register',
 		Refresh: 'account/refresh',
 	}
