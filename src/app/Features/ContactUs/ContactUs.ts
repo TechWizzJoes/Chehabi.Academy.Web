ce1701a7cf1dd9396d7ad50e979060043791ba95
Yousif Whby <yousifwhby@gmail.com>
2023-11-01 22:29:53 +0200

    contact us page simi complete

--- src/app/Features/ContactUs/ContactUs.ts
+++ src/app/Features/ContactUs/ContactUs.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { AuthService } from '@App/Common/Services/Auth.Service';
 import { NotifyService } from '@App/Common/Services/Notify.Service';
@@ -14,9 +14,10 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 	standalone: true,
 	templateUrl: './ContactUs.html',
 	styleUrls: ['ContactUs.scss'],
-	imports: [FormsModule, CommonModule, NgxChartsModule]
+	imports: [FormsModule, CommonModule, NgxChartsModule, ReactiveFormsModule]
 })
 export class ContactUsComponent implements OnInit {
+	contactForm: FormGroup;
 	constructor(
 		private router: Router,
 		private route: ActivatedRoute,
@@ -24,8 +25,23 @@ export class ContactUsComponent implements OnInit {
 		private ErrorCodesService: ErrorCodesService,
 		private NotifyService: NotifyService,
 		private AuthService: AuthService,
-		private StorageService: StorageService
-	) { }
+		private StorageService: StorageService,
+		private fb: FormBuilder
+	) {
+		this.contactForm = this.fb.group({
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
+			age: ['', [Validators.required, Validators.min(1)]],
+			description: ['', Validators.required]
+		});
+	}
 
-	ngOnInit() { }
+	onSubmit() {
+		if (this.contactForm.valid) {
+			// Handle form submission here
+			console.log(this.contactForm.value);
+		}
+	}
+
+	ngOnInit() {}
 }
