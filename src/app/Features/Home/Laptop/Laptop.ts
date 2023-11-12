886bb61fe377c94c14d34893da09f3fa78b19f67
youssefmansour <ymansour@ultatel.com>
2023-11-12 22:18:13 +0200

    WEB-26
    landing page animation initial

---
+++ src/app/Features/Home/Laptop/Laptop.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '@App/Common/Services/Auth.Service';
+import { NotifyService } from '@App/Common/Services/Notify.Service';
+import { ErrorCodesService } from '@App/Common/Services/ErrorCodes.Service';
+import { StorageService } from '@App/Common/Services/Storage.Service';
+import { HttpService } from '@App/Common/Services/Http.Service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { RoutePaths } from '@App/Common/Settings/RoutePaths';
+
+@Component({
+	standalone: true,
+	selector: 'app-landing-laptop',
+	templateUrl: './Laptop.html',
+	styleUrls: ['Laptop.scss'],
+	imports: [FormsModule, CommonModule, RouterModule, NgxChartsModule, TranslateModule, NgbCarouselModule]
+})
+export class LaptopComponent implements OnInit {
+	constructor(
+		private Router: Router,
+		private ActivatedRoute: ActivatedRoute,
+		private HttpService: HttpService,
+		private ErrorCodesService: ErrorCodesService,
+		private NotifyService: NotifyService,
+		private AuthService: AuthService,
+		private StorageService: StorageService
+	) {
+	}
+
+	ngOnInit() { }
+}
